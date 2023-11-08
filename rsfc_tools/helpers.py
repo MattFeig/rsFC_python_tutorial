4841be15affe323430ca62f709a87b38ef7bd34b
MattFeig <30275344+MattFeig@users.noreply.github.com>
2023-11-08 10:24:41 -0800

    tutorial material refactor

--- rsfc_tools/helpers.py
+++ rsfc_tools/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os,subprocess
 
 def create_corrmat(arr):
     """
@@ -22,3 +23,33 @@ def create_corrmat(arr):
     corrmat = np.arctanh(corrmat)
     np.fill_diagonal(corrmat, 1)
     return corrmat
+
+# This requires having connectome workbench installed
+# You may need to edit the path_to_wb variable with your own local path to the workbench installation
+def save_nii(array, output_name, output_dir_path, wb_required_template_path, purge = True):
+    
+    path_to_wb = '/Applications/workbench/bin_macosx64/wb_command'
+    
+    '''
+    Save a numpy array as a .nii file, utilizing workbench as an intermediary.
+    Arguments:
+        array: the numpy array to save
+        output_name: the name to use for the output file
+        output_dir_path: the directory path to save the output file in
+        wb_required_template_path: a dtseries of the same dimension used as a template to write over
+        purge: if True, delete the intermediate text file after creating the .nii file
+    '''
+    if not os.path.isdir(output_dir_path):
+        raise Exception(f"The output folder {output_dir_path} does not exist")
+    template_base = os.path.basename(wb_required_template_path)
+    end = template_base.find('.')
+    file_end = template_base[end:]
+    out_path = os.path.join(output_dir_path, output_name)
+    outnamecifti = out_path+file_end
+    if os.path.isfile(outnamecifti):
+        print('-WARNING: Overwriting')
+    np.savetxt(out_path, array)
+    wb_comm = ' '.join([path_to_wb, '-cifti-convert -from-text', out_path, wb_required_template_path, outnamecifti])
+    subprocess.call(wb_comm, shell=True)
+    if purge:
+        os.remove(out_path)        
